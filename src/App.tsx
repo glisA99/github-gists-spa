f03263a393574e0b8f704cafde2511fe5767e71e
glisA99 <ognjen.simic99@gmail.com>
2022-07-10 23:23:30 +0200

    Added navbar component. Enabling dynamic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
 import React from "react";
+import { Route, Routes } from "react-router";
 import "./App.css";
 import GistList from "./components/GistList";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <div className="header-div">
-        <p style={{ margin: 0 }}><b>Gists</b></p>
-      </div>
-      <div className="content-div">
-        <GistList />
-      </div>
-      <div id="fade-in-image" />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          <React.Fragment>asdasdads</React.Fragment>
+        }>
+        </Route>
+        <Route
+          path="/gists"
+          element={
+            <React.Fragment>
+              <div className="header-div">
+                <p style={{ margin: 0 }}>
+                  <b>Gists</b>
+                </p>
+              </div>
+              <div className="content-div">
+                <GistList />
+              </div>
+              <div id="fade-in-image" />
+            </React.Fragment>
+          }
+        ></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Navbar = () => {
+  
+    return (
+        <div className='navbar'>
+            <h2 className='navbar-h'>GitHub Gists</h2>
+            <div className='navbar-item'>
+                Home
+            </div>
+            <div className='navbar-item'>
+                Gists
+            </div>
+        </div>
+    )
+
+}
